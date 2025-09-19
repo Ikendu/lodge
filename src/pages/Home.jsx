865e99a39ef165b1ffbaa1143739d2db7e3513bc
David Aniede <ndubest56@gmail.com>
2025-09-19 11:17:57 +0100

    building components and pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex bluer-500 h-screen justify-center items-center gap-20">
+      <div className="">
+        <div>Lodge Seeker</div>
+        <div>Are you seeking for lodge or apartment</div>
+        <div>Click here</div>
+      </div>
+      <div>
+        <div>Lodge Owner</div>
+        <div>Do you have a lodge, space or apartment to rent out</div>
+        <div>Click here</div>
+      </div>
+    </div>
+  );
 }
