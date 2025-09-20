65749ae79a0ea5da1dbdf457ba9a7c02fd7a35d3
David Aniede <ndubest56@gmail.com>
2025-09-20 05:12:22 +0100

    landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,23 @@
 import guest from "../assets/logos/guest.png";
-import owner from "../assets/logos/owner.png";
+import ownerh from "../assets/logos/ownerh.png";
 export default function Home() {
   return (
     <div className="flex blue-500 h-screen justify-center items-center gap-20">
-      <div className="">
-        <div>
+      <div className="bigbtn">
+        <div className="btnhead">
           <img src={guest} alt="guest or tenant" className="max-h-20" />
           <h4>Lodge Seeker</h4>
         </div>
-        <div>Are you seeking for lodge or apartment</div>
-        <div>Click here</div>
+        <div>Are you a traveler or a tenant seeking for lodge or apartment</div>
+        <div className="text-black">Click here</div>
       </div>
-      <div>
-        <div>
-          <img src={owner} alt="guest or tenant" className="max-h-20" />
+      <div className="bigbtn">
+        <div className="btnhead">
+          <img src={ownerh} alt="guest or tenant" className="max-h-20" />
           <h4>Lodge Owner</h4>
         </div>
         <div>Do you have a lodge, space or apartment to rent out</div>
-        <div>Click here</div>
+        <div className="text-black">Click here</div>
       </div>
     </div>
   );
